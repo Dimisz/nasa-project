33203889874035ff0047e12db40267551b2f8c01
Vladimir Solovyov <leilyngale@icloud.com>
2025-10-09 11:59:05 +0800

    Write custom lightweight cors middleware + added first endpoint to get all planets

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import planetsRouter from './routes/planets/planets.router';
+
+const CLIENT_URL = 'http://localhost:3000';
 const app = express();
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  res.header('Access-Control-Allow-Origin', CLIENT_URL);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.use(express.json());
+app.use(planetsRouter);
+
 
 export default app;
---
+++ api/src/routes/planets/planets.router.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { getAllPlanets } from './planets.controller';
+
+const planetsRouter = Router();
+
+planetsRouter.get('/planets', getAllPlanets);
+
+export default planetsRouter;
