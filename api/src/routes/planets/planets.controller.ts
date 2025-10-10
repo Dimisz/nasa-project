f0b623f8a63993a06a721aa8e68f036fa9e8dde7
Vladimir Solovyov <leilyngale@icloud.com>
2025-10-10 20:19:22 +0800

    Add loading planets from csv file functionality on startup

--- api/src/routes/planets/planets.controller.ts
+++ api/src/routes/planets/planets.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import planets from "../../models/planets.model";
+import { planets } from "../../models/planets.model";
 
 export function getAllPlanets(_req: Request, res: Response) {
   return res.status(200).json(planets);
